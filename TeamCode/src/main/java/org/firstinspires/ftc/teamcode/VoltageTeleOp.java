8e2fac7662f264ae38cc6170790795c1127ead52
Jason Horn <jason7horn@gmail.com>
2016-11-04 13:10:50 -0400

    updated teleop to include gamepad control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VoltageTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VoltageTeleOp.java
@@ -1,10 +1,18 @@
 package org.firstinspires.ftc.teamcode;
 
-//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+
+@TeleOp(name = "VoltageTeleOp",group = "VoltageTeleOp")
 
 public class VoltageTeleOp extends OpMode {
 
@@ -22,10 +30,24 @@ public class VoltageTeleOp extends OpMode {
     public void loop() {
         double threshold = 0.1;
         telemetry.addData("Joystick_1","Y_1:"+Double.toString(gamepad1.left_stick_y));
-        telemetry.addData("Joystick_1","Y_2:"+Double.toString(gamepad1.right_stick_y));
+        telemetry.addData("Joystick_2","Y_2:"+Double.toString(gamepad1.right_stick_y));
         if(Math.abs(gamepad1.left_stick_y)>threshold)
         {
             Left.setPower(gamepad1.left_stick_y);
         }
+        else {
+            Left.setPower(0.0);
+        }
+
+        if(Math.abs(gamepad1.right_stick_y)>threshold)
+        {
+            Right.setPower(gamepad1.right_stick_y);
+        }
+        else {
+            Right.setPower(0.0);
+        }
+
     }
+
+
 }
